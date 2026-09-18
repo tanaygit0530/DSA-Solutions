9a887f420ab5639e553e305c8750d03ab87d884b
Tanay Patil <tanaypatil0530@gmail.com>
2026-09-18 19:08:03 +0530

    Update LeetCode: 3sum - Code

--- Arrays/3sum/solution.java
+++ Arrays/3sum/solution.java
@@ -1,1 +1,23 @@
-// Captured code
+                    j = j-1;                }            }        }    }    public List<List<Integer>> threeSum(int[] nums) {        Arrays.sort(nums);        List<List<Integer>> res = new ArrayList<>();        for(int f = 0; f<nums.length; f++){            if(nums[f]>0){                break;            }            if(f == 0 || nums[f] != nums[f-1]){                twoSum(f,nums,res);            }        }            return res;    }}                while(i<j && nums[j] == nums[j+1]){                }
+                    j = j-1;
+                }
+            }
+        }
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int f = 0; f<nums.length; f++){
+            if(nums[f]>0){
+                break;
+            }
+            if(f == 0 || nums[f] != nums[f-1]){
+                twoSum(f,nums,res);
+            }
+        }
+            return res;
+    }
+}
+                while(i<j && nums[j] == nums[j+1]){
+                }
+
