1c4ee3ffab61fe73f0e705c6df62ebec77a4857b
Tanay Patil <tanaypatil0530@gmail.com>
2026-09-18 19:10:45 +0530

    Update LeetCode: 3sum - Code

--- Arrays/3sum/solution.java
+++ Arrays/3sum/solution.java
@@ -1,12 +1,17 @@
-                    j = j-1;                }            }        }    }    public List<List<Integer>> threeSum(int[] nums) {        Arrays.sort(nums);        List<List<Integer>> res = new ArrayList<>();        for(int f = 0; f<nums.length; f++){            if(nums[f]>0){                break;            }            if(f == 0 || nums[f] != nums[f-1]){                twoSum(f,nums,res);            }        }            return res;    }}                while(i<j && nums[j] == nums[j+1]){                }
+                }                while(i<j && nums[j] == nums[j                +1]){                    j = j-1;                }            }        }    }    public List<List<Integer>> threeSum(int[]     nums) {        Arrays.sort(nums);        List<List<Integer>> res = new         ArrayList<>();        for(int f = 0; f<nums.length; f++){            if(nums[f]>0){                break;            }            if(f == 0 || nums[f] != nums[f-1]){                twoSum(f,nums,res);            }        }            return res;    }
+                }
+                while(i<j && nums[j] == nums[j
+                +1]){
                     j = j-1;
                 }
             }
         }
     }
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum(int[] 
+    nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new 
+        ArrayList<>();
         for(int f = 0; f<nums.length; f++){
             if(nums[f]>0){
                 break;
@@ -17,7 +22,4 @@
         }
             return res;
     }
-}
-                while(i<j && nums[j] == nums[j+1]){
-                }
 
